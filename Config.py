a619268d57bab8543c2642cd359e38b076824010
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 14:10:18 +0200

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,13 +1,8 @@
 import os
 
-class Config(object):
-     
-    BOT_TOKEN = ("5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0")
-
-
-    API_ID = ("13472617",))
-
-
-    API_HASH = ("deb30aa6abbfca7d4cdffaec397edbc1",)
-
+BOTidSBot=Client(
+    "Pyrogram Bot", 
+    bot_token="5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0", 
+    api_id="13472617", 
+    api_hash="deb30aa6abbfca7d4cdffaec397edbc1"
 
