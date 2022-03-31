8b7320b16f5724435e2388d97c3e27daf6b00f21
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 09:36:22 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,18 +1,18 @@
 from pyrogram import Client, filters
 
 
-JESUS=Client(
+bot=Client(
     "Pyrogram Bot", 
     bot_token="5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0", 
     api_id="13472617", 
     api_hash="deb30aa6abbfca7d4cdffaec397edbc1"
 )
 
-@JESUS.on_message(filters.command("start"))
+@bot.on_message(filters.command("start"))
 async def startmsg(bot, message):
     await message.reply_video(video="https://telegra.ph/file/b8f0cbdf67943328459d2.mp4", 
     caption=f"Hello")
 
 
 
-JESUS()
+bot.run()
