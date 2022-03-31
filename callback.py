ac19ed1f5fa1e9b5643b8311dd730dd27dada736
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 13:54:42 +0200

    Update callback.py

--- callback.py
+++ callback.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram import Client as BOTidSBot
 from BOTidSBot.Translation import Translation
-from BOTidSBot.echobot import echobot
+from BOTidSBot.Config import Config
 
 BOT_USERNAME=Config.BOT_USERNAME
 
