31fd3c790fc7365642581b0120d8eda7f77ac5c6
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 10:30:57 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -13,7 +13,7 @@ async def startmsg(bot, message):
           reply_markup=InlineKeyboardMarkup( [[
          InlineKeyboardButton("channel", url=f"https://t.me/robloxXO")
          ]]
-       )
+        )
    )
 
     await message.reply_text("hello")
