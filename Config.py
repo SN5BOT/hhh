91fb83fa9b3783c18ca83691fff41a4c51ba2763
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 14:13:26 +0200

    Delete Config.py

--- Config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-BOTidSBot=Client(
-    "Pyrogram Bot", 
-    bot_token="5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0", 
-    api_id="13472617", 
-    api_hash="deb30aa6abbfca7d4cdffaec397edbc1"
-
