3150388dd0624d6c87a54cd33e1c61cdda03b428
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 13:31:28 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -13,19 +13,18 @@ async def start_message(bot, message):
     await message.reply_text(
         text="links",
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton("Button1", url=f"t.me/us7a5")
-            InlineKeyboardButton("Button2", url=f"t.me/us7a5")
+            InlineKeyboardButton("Button1", url=f"t.me/us7a5"),
+            InlineKeyboardButton("Button2", url=f"t.me/us7a5"),
             InlineKeyboardButton("Button3", url=f"t.me/us7a5")
             ],[
             InlineKeyboardButton("Button4", url=f"t.me/us7a5")
             ],[
-            InlineKeyboardButton("Button5", url=f"t.me/us7a5")
-            InlineKeyboardButton("Button6", url=f"t.me/us7a5")
+            InlineKeyboardButton("Button5", url=f"t.me/us7a5"),
+            InlineKeyboardButton("Button6", url=f"t.me/us7a5"),
             InlineKeyboardButton("Button7", url=f"t.me/us7a5")
             ]]
-          )
-      )
-
-    
+            )
+        )
+   
 
 bot.run()
