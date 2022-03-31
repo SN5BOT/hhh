064fd37bfe5c4b262fc18986d372bdbbfb45fcef
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 08:27:38 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,14 +1,14 @@
-from pyrogram import bot, filters
+
 from pyrogram import Client, filters
 from pyrogram.types
 import pyfiglet
 import telebot
 
 bot = telebot.TeleBot ("5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0")
-@bot.message_handler(commands=['start'])
+@Client.message_handler(commands=['start'])
 def first_msg(message):
     bot.send_message(message.chat.id,text=f'<b>Hi {message.from_user.first_name}\n- - - - - - - -\nWelcome To Terminal Name Bot\nSend Your Name To Make It\n- - - - - - - -\nBy : @Vodka_Tools</b>',parse_mode='html')
-@bot.message_handler(func=lambda message:True)
+@Client.message_handler(func=lambda message:True)
 def name(message):
     name = message.text
     design = pyfiglet.figlet_format(name)
