469afc1b6a926a16712645ad466b288ddca3d5a3
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 08:34:21 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,16 +1,22 @@
-
 from pyrogram import Client, filters
+from googletrans import Translator
 from pyrogram.types
 import pyfiglet
 import telebot
+from googletrans import Translator
 
-bot = telebot.TeleBot ("5131732775:AAGBIP8WByqCGeiO-bElbvc7XbjS_P1l7_0")
-@Client.message_handler(commands=['start'])
+bot = Client("Chatbot", 
+                bot=getenv("telebot.TeleBot")
+
+tr = Translator()
+
+
+@bot.message_handler(commands=['start'])
 def first_msg(message):
     bot.send_message(message.chat.id,text=f'<b>Hi {message.from_user.first_name}\n- - - - - - - -\nWelcome To Terminal Name Bot\nSend Your Name To Make It\n- - - - - - - -\nBy : @Vodka_Tools</b>',parse_mode='html')
-@Client.message_handler(func=lambda message:True)
+@bot.message_handler(func=lambda message:True)
 def name(message):
     name = message.text
     design = pyfiglet.figlet_format(name)
     bot.send_message(message.chat.id,text=design)
-bot.polling(True)
+bot.run()
