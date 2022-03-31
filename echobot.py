da8c4928a494f08e9761abb7e09c730a09d584e9
SN5BOT <102501213+SN5BOT@users.noreply.github.com>
2022-03-31 13:12:58 +0200

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -11,6 +11,7 @@ bot=Client(
 @bot.on_message(filters.command("^start"))
 async def start_message(bot, message):
     await message.reply_text(
+    await message.reply_video(video="https://telegra.ph/file/b8f0cbdf67943328459d2.mp4",
         text="Hello",
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton("channel", url=f"https://t.me/robloxXO")
